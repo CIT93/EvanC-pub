e827989f7a265b737d5d30ed1a42a599f69f7a9c
Evan-Cortes <110888284+Evan-Cortes@users.noreply.github.com>
2022-08-18 22:07:50 -0700

    Create W2LearnTogether.js

---
+++ W2LearnTogether.js
@@ -0,0 +1,11 @@
+let airQuality = 99
+
+if (airQuality >= 100) {
+    console.log('Air is bad, dont go for a run today')
+} else if(airQuality >= 50 && airQuality < 100) {
+    console.log('Air Quality is okay, maybe cut the run short today')
+} else {
+    console.log('Air Quality is good, go for a run!')
+}
+    
+

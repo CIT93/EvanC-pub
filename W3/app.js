d1a585e5243ce997e51fe6f798764a154b0472f1
Evan-Cortes <110888284+Evan-Cortes@users.noreply.github.com>
2022-08-24 13:03:28 -0700

    Update app.js

--- W3/app.js
+++ W3/app.js
@@ -5,19 +5,18 @@ const showOnPage = function (text) {
     outputDiv.append(newParagraph);
 };
 
-let AirQualityChecker = function (airNumber) { // This Function associates Air Quality Numbers to standard Air QUality Colors
-    if (airNumber >= 100) {
-        airQuality = 'Orange'
-    } else if(airNumber >= 50 && airNumber < 100) {
-        airQuality = 'Yellow'
-    } else {
-        airQuality = 'Green'
-    }
-}
 
 let workoutSummarizer = function (airValue, exercise = 'Gym') { // By Default the exercise is the gym
+    let AirQualityChecker = function (airNumber) { // This Function associates Air Quality Numbers to standard Air QUality Colors
+        if (airNumber >= 100) {
+            airQuality = 'Orange'
+        } else if(airNumber >= 50 && airNumber < 100) {
+            airQuality = 'Yellow'
+        } else {
+            airQuality = 'Green'
+        }
+    }
     let airQuality = AirQualityChecker(airValue) // This line runs the AirQualityChecker function to give us the Air Quality
-    
     if (exercise == 'Jog' && airQuality == 'Orange') {
         summary = 'Air Quality is bad, dont go for a jog'
     } else if(exercise == "Jog" && airQuality == 'Yellow') {
